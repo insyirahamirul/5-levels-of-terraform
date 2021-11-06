b2b46841c0c39b1a6a01b8cf399e905c77d7987c
Insyirah Amirul <insyirahamirulchang@gmail.com>
2021-11-06 21:40:33 +0800

    added app service and sql db resources

--- azure-level2/main.tf
+++ azure-level2/main.tf
@@ -15,6 +15,56 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = "westus2"
+  name     = "myapp-resourcegroup"
+  location = "southeastasia"
+}
+
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "myapp-appserviceplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "myapp-appservice"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  //Getting the ID of app service plan created in line 22-31
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "Server=tcp:azurerm_sql_server.sqldb.fully_qualified_domain_name Database=azurerm_sql_database.db.name;User ID=azurerm_sql_server.sqldb.administrator_login;Password=azurerm_sql_server.sqldb.administrator_login_password;Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "sqldb" {
+  name                         = "myapp-sqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "admin"
+  administrator_login_password = "@dm!n123"
+}
+
+resource "azurerm_sql_database" "db" {
+  name                = "terraform-sqldatabase"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sqldb.name
 }
