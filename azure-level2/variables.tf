3a9bbe4946870e3001b426a1703c938c1367779b
insyirahamirul <insyirahamirulchang@gmail.com>
2021-11-06 22:38:03 +0800

    Update variables.tf
    added new vars

--- azure-level2/variables.tf
+++ azure-level2/variables.tf
@@ -1,3 +1,19 @@
 variable "resource_group_name" {
-  type = strings
+  type = string
+}
+
+variable "app_service_plan_name" {
+  type = string
+}
+
+variable "app_service_name" {
+  type = string
+}
+
+variable "sql_server_name" {
+  type = string
+}
+
+variable "sql_db_name" {
+  type = string
 }
